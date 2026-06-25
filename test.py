9668baae321e1273c843e5a225b25d61e6e36d5c
Yash nadheriya <yashnadheriya@gmail.com>
2026-06-25 12:56:59 +0530

    Move database credentials to environment variables

--- test.py
+++ test.py
@@ -1,9 +1,10 @@
 import mysql.connector
 
 conn = mysql.connector.connect(
-    host="localhost",
-    user="root",
-    password="your password"
+    host=os.getenv("DB_HOST"),
+    user=os.getenv("DB_USER"),
+    password=os.getenv("DB_PASSWORD"),
+    database=os.getenv("DB_NAME")
 )
 
 print("Connected Successfully")
